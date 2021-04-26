68bfbcdc9ee6473c737a31e0da89ab92289ff91a
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-26 16:18:50 +0200

    Sistemato esercizio Mail

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -1,21 +1,20 @@
 //chiedo all'utente la sua mail
-var mailUtente = prompt("Inserisci la tua mail!");
+var mailUtente = prompt("Inserisci la tua mail! \nAttenzione si tiene conto delle maiuscole e delle minuscole!");
 document.getElementById("mail_inserita").innerHTML = mailUtente;
-//controllo che sia nella lista di chi puo accedere
 
+
+//controllo che sia nella lista di chi puo accedere
 var listaPartecipanti = ["mirco@gmail.com", "stefano_rossi@libero.it", "mauro.fornasiero@outlook.com", "pincopallino19@simpatico.net", "andreiburbulia@hotmail.it"]
-console.log(listaPartecipanti);
 
 
 var controllo = listaPartecipanti.indexOf(mailUtente);
-console.log(controllo);
 
 if (controllo < listaPartecipanti.length && controllo >= 0) {
-    document.getElementById("controllo_mail").innerHTML = "La tua mail è presente nella lista";
+    document.getElementById("controllo_mail").innerHTML = "Si!";
     document.getElementById("esito_mail").innerHTML = "Puoi accedere!";
 
 } else {
-    document.getElementById("controllo_mail").innerHTML = "La tua mail non è presente nella lista";
+    document.getElementById("controllo_mail").innerHTML = "No!";
     document.getElementById("esito_mail").innerHTML = "Non puoi accedere!";
 }
 
